b68b38f848dae518ad565b68bd67e85d6a066ef8
Zainur Roziqin <75110898+zainurroziqin@users.noreply.github.com>
2025-06-22 14:25:11 +0800

    Update

--- notes-app-backend/src/server.js
+++ notes-app-backend/src/server.js
@@ -4,7 +4,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: 'rozidev',
         routes: { cors: { origin: ['*'] } },
     });
 
